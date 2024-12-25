097f68311222667c9cea65e32440b9e3b63b14d0
kayatsa <kayatsa8@gmail.com>
2024-12-25 20:58:23 +0200

    initializing HomeDishes MS.

---
+++ HomeDishes/src/main/java/com/cookbook/homedishes/HomedishesApplication.java
@@ -0,0 +1,13 @@
+package com.cookbook.homedishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomedishesApplication.class, args);
+	}
+
+}
