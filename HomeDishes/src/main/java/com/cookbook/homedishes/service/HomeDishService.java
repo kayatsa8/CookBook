accd67b4cac7ca3f68c6d7e1659d74c008cd28f4
kayatsa <kayatsa8@gmail.com>
2025-01-05 22:17:27 +0200

    HomeDishes/src/main/java/com/cookbook/homedishes/service/HomeDishService: adding service.

---
+++ HomeDishes/src/main/java/com/cookbook/homedishes/service/HomeDishService.java
@@ -0,0 +1,47 @@
+package com.cookbook.homedishes.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cookbook.homedishes.exception.DishExistsException;
+import com.cookbook.homedishes.exception.IlligalDishException;
+import com.cookbook.homedishes.model.HomeDish;
+import com.cookbook.homedishes.repository.HomeDishRepository;
+
+@Service
+public class HomeDishService {
+
+    @Autowired
+    private HomeDishRepository repo;
+
+
+
+    public void addDish(HomeDish dish) throws DishExistsException, IlligalDishException{        
+        if(isDishExists(dish.getName())){
+            throw new DishExistsException(dish.getName());
+        }
+
+        validateDishData(dish);
+
+
+    }
+
+
+
+
+    private boolean isDishExists(String name){
+        return repo.findById(name).isPresent();
+    }
+
+    private void validateDishData(HomeDish dish) throws IlligalDishException{
+        if(dish.getName() == null || dish.getName().isEmpty()){
+            throw new IlligalDishException("the dish name is empty");
+        }
+
+        if(dish.getType() == null){
+            throw new IlligalDishException("the type of the dish must be specified");
+        }
+    }
+
+    
+}
