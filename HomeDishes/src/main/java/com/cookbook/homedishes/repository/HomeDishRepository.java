12996b68aa3a9f8a17eb24730b3f0ecb155bc483
kayatsa <kayatsa8@gmail.com>
2025-01-05 21:58:42 +0200

    .

--- HomeDishes/src/main/java/com/cookbook/homedishes/repository/HomeDishRepository.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/repository/HomeDishRepository.java
@@ -1,9 +1,11 @@
 package com.cookbook.homedishes.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cookbook.homedishes.model.HomeDish;
 
+@Repository
 public interface HomeDishRepository extends MongoRepository<HomeDish, String>{
     
 }
