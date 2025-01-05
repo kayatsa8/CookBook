4ea1448686d337b0f64a40e39c369ae07367318c
kayatsa <kayatsa8@gmail.com>
2025-01-05 21:33:56 +0200

    HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish: fields, constructors, getters, setters

--- HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish.java
@@ -1,5 +1,51 @@
 package com.cookbook.homedishes.Model;
 
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.cookbook.homedishes.Model.enums.Difficulty;
+import com.cookbook.homedishes.Model.enums.DishType;
+import com.cookbook.homedishes.Model.enums.Flavors;
+import com.cookbook.homedishes.Model.enums.MealPart;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Document("HomeDish")
+@Getter
+@Setter
 public class HomeDish {
-    
+    @Id
+    private int id;
+    private String name;
+    private int diners;
+    private String recipe;
+    private Set<String> ingredients;
+    private int timeInMinutes;
+    private DishType type;
+    private Set<Flavors> flavors;
+    private Difficulty difficulty;
+    private MealPart mealPart;
+    private int rating;
+
+
+    public HomeDish(String name, int diners, String recipe, Set<String> ingredients,
+                    int timeInMinutes, DishType type, Set<Flavors> flavors,
+                    Difficulty difficulty, MealPart mealPart, int rating){
+
+        this.name = name;
+        this.diners = diners;
+        this.recipe = recipe;
+        this.ingredients = ingredients;
+        this.timeInMinutes = timeInMinutes;
+        this.type = type;
+        this.flavors = flavors;
+        this.difficulty = difficulty;
+        this.mealPart = mealPart;
+        this.rating = rating;
+    }
+
+
 }
