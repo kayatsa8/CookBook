bfa811e348987b464d89d5f1d8a8d1b781097db0
kayatsa <kayatsa8@gmail.com>
2025-01-06 13:28:18 +0200

    HomeDish: update from dto method

--- HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.cookbook.homedishes.model.dto.DTO;
 import com.cookbook.homedishes.model.enums.Difficulty;
 import com.cookbook.homedishes.model.enums.DishType;
 import com.cookbook.homedishes.model.enums.Flavors;
@@ -44,6 +45,19 @@ public class HomeDish {
         this.difficulty = difficulty;
         this.mealPart = mealPart;
         this.rating = rating;
+    }
+
+    public void updateFromDTO(DTO dto){
+        name = dto.getName() == null ? name : dto.getName();
+        diners = dto.getDiners() == 0 ? diners : dto.getDiners();
+        recipe = dto.getRecipe() == null ? recipe : dto.getRecipe();
+        ingredients = dto.getIngredients() == null ? ingredients : dto.getIngredients();
+        timeInMinutes = dto.getTimeInMinutes() == 0 ? timeInMinutes : dto.getTimeInMinutes();
+        type = dto.getType() == null ? type : dto.getType();
+        flavors = dto.getFlavors() == null ? flavors : dto.getFlavors();
+        difficulty = dto.getDifficulty() == null ? difficulty : dto.getDifficulty();
+        mealPart = dto.getMealPart() == null ? mealPart : dto.getMealPart();
+        rating = dto.getRating() == 0 ? rating : dto.getRating();
     }
 
 
