d46f6900574f505ef5d128fe367569a134dedbdd
kayatsa <kayatsa8@gmail.com>
2025-01-08 23:04:21 +0200

    homedishes/controller/HomeDishController: update dish

--- HomeDishes/src/main/java/com/cookbook/homedishes/controller/HomeDishController.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/controller/HomeDishController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -65,6 +66,18 @@ public class HomeDishController {
         try{
             HomeDish dish = service.getDish(dishName);
             return dish;
+        }
+        catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/update/{originalName}")
+    public void updateDish(@PathVariable String originalName, @RequestBody HomeDish updated){
+        // TODO: validate the data
+        try{
+            service.updateDish(originalName, updated);
         }
         catch(Exception e){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
