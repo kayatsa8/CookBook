0cc0aa18e4d3dd69c7236f46aacf977d0401acef
kayatsa <kayatsa8@gmail.com>
2025-01-30 10:56:57 +0200

    adding DeliveryDishRepository

---
+++ DeliveryFood/src/main/java/com/cookbook/deliveryfood/repository/DeliveryDishRepository.java
@@ -0,0 +1,11 @@
+package com.cookbook.deliveryfood.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cookbook.deliveryfood.model.DeliveryDish;
+
+@Repository
+public interface DeliveryDishRepository extends CrudRepository<DeliveryDish, Integer>{
+    
+}
