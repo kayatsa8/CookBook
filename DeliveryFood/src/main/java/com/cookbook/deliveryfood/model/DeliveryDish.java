6a2879832ca651cb5d74dc77345a0b125912f3c2
kayatsa <kayatsa8@gmail.com>
2025-01-30 12:40:14 +0200

    deliveryfood/model/DeliveryDish: added update()

--- DeliveryFood/src/main/java/com/cookbook/deliveryfood/model/DeliveryDish.java
+++ DeliveryFood/src/main/java/com/cookbook/deliveryfood/model/DeliveryDish.java
@@ -27,13 +27,26 @@ public class DeliveryDish {
 
     private String name;
     private Double price;
-    private String Restaurant;
+    private String restaurant;
     private String deliveryPlatform;
     private Integer rating;
     private DishType type;
     private List<Flavors> flavors;
     private MealPart mealPart;
     
+
+
+    public void update(DeliveryDish other){
+        name = other.name == null ? name : other.name;
+        price = other.price == null ? price : other.price;
+        restaurant = other.restaurant == null ? restaurant : other.restaurant;
+        deliveryPlatform = other.deliveryPlatform == null ? deliveryPlatform : other.deliveryPlatform;
+        rating = other.rating == null ? rating : other.rating;
+        type = other.type == null ? type : other.type;
+        flavors = other.flavors == null ? flavors : other.flavors;
+        mealPart = other.mealPart == null ? mealPart : other.mealPart;
+    }
+
 
     
 }
