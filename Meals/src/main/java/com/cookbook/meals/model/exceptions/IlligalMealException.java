87a8854896ce82125661313c35df1374959374a5
kayatsa <kayatsa8@gmail.com>
2025-02-04 16:09:24 +0200

    new Exception

---
+++ Meals/src/main/java/com/cookbook/meals/model/exceptions/IlligalMealException.java
@@ -0,0 +1,9 @@
+package com.cookbook.meals.model.exceptions;
+
+public class IlligalMealException extends Exception{
+    
+    public IlligalMealException(String message){
+        super(message);
+    }
+
+}
