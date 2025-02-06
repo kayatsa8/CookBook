7c17710a46a4f39dc357a747dc9e94cf7440c6e7
kayatsa <kayatsa8@gmail.com>
2025-02-06 21:42:30 +0200

    deliveryfood/service/InternalDishService: adding methods to check id a dish or a list of dishes exist

--- DeliveryFood/src/main/java/com/cookbook/deliveryfood/service/InternalDishService.java
+++ DeliveryFood/src/main/java/com/cookbook/deliveryfood/service/InternalDishService.java
@@ -1,5 +1,40 @@
 package com.cookbook.deliveryfood.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cookbook.deliveryfood.repository.DeliveryDishRepository;
+
+@Service
 public class InternalDishService {
+    private final DeliveryDishRepository repo;
+
+
+    @Autowired
+    public InternalDishService(DeliveryDishRepository repository){
+        this.repo = repository;
+    }
+
+    public boolean isDishExists(int id){
+        return repo.existsById(id);
+    }
+
+    public List<Integer> checkDishList(List<Integer> ids){
+        List<Integer> badIds = new ArrayList<>();
+
+        for(int id : ids){
+            if(!repo.existsById(id)){
+                badIds.add(id);
+            }
+        }
+
+        return badIds;
+    }
+
+
+
     
 }
