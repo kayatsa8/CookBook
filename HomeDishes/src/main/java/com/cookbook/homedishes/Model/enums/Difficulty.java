c737f38f315184a133382c16bc2a9f8feddc9fcc
kayatsa <kayatsa8@gmail.com>
2025-02-07 16:05:14 +0200

    homedishes/Model/enums/Difficulty: adding int values to enum

--- HomeDishes/src/main/java/com/cookbook/homedishes/Model/enums/Difficulty.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/Model/enums/Difficulty.java
@@ -1,5 +1,16 @@
 package com.cookbook.homedishes.model.enums;
 
 public enum Difficulty {
-    EASY, NORMAL, HARD
+    EASY(0), NORMAL(1), HARD(2);
+
+
+    private final int value;
+
+    Difficulty(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
