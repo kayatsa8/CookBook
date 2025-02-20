3ba3bf0df52d79941aef097899adae17460d778c
kayatsa <kayatsa8@gmail.com>
2025-02-20 19:14:42 +0200

    homedishes/Model/HomeDish: removing redundant imports

--- HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/Model/HomeDish.java
@@ -10,7 +10,6 @@ import com.cookbook.homedishes.model.enums.DishType;
 import com.cookbook.homedishes.model.enums.Flavors;
 import com.cookbook.homedishes.model.enums.MealPart;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
