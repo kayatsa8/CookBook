7af786fe2170356c6049884ccd2bba295c2064d9
kayatsa <kayatsa8@gmail.com>
2025-02-20 21:54:46 +0200

    cookbook/homedishes/Model/CookTime: CookTime implementes Compareable

--- HomeDishes/src/main/java/com/cookbook/homedishes/Model/CookTime.java
+++ HomeDishes/src/main/java/com/cookbook/homedishes/Model/CookTime.java
@@ -9,9 +9,28 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class CookTime {
+public class CookTime implements Comparable<CookTime>{
     private Integer days;
     private Integer hours;
     private Integer minutes;
     private Integer seconds;
+
+
+
+    @Override
+    public int compareTo(CookTime other) {
+        if(this.days - other.days != 0){
+            return this.days - other.days;
+        }
+
+        if(this.hours - other.hours != 0){
+            return this.hours - other.hours;
+        }
+
+        if(this.minutes - other.minutes != 0){
+            return this.minutes - other.minutes;
+        }
+
+        return this.seconds - other.seconds;
+    }
 }
