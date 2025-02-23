37bca9894e89f1785a55ac9f32f03463dab07e27
kayatsa <kayatsa8@gmail.com>
2025-02-23 19:19:16 +0200

    deliveryfood/model/DTO: adding DTO for partial retrival

---
+++ DeliveryFood/src/main/java/com/cookbook/deliveryfood/model/DTO.java
@@ -0,0 +1,15 @@
+package com.cookbook.deliveryfood.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DTO {
+    private Integer id;
+    private String name;
+}
