063157eccc1148617618d049777902ac986fd350
kayatsa <kayatsa8@gmail.com>
2025-02-27 10:36:36 +0200

    init MealGenerator

---
+++ MeatGenerator/src/main/java/com/cookbook/meatgenerator/MeatgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.cookbook.meatgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatgeneratorApplication.class, args);
+	}
+
+}
