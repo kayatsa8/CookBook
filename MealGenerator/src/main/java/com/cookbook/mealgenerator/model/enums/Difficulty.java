71e33e7168994726efa77c010821ba187fb41c9c
kayatsa <kayatsa8@gmail.com>
2025-02-27 11:33:16 +0200

    MealGenerator: adding Difficulty enum

---
+++ MealGenerator/src/main/java/com/cookbook/mealgenerator/model/enums/Difficulty.java
@@ -0,0 +1,16 @@
+package com.cookbook.mealgenerator.model.enums;
+
+public enum Difficulty {
+    EASY(0), NORMAL(1), HARD(2);
+
+
+    private final int value;
+
+    Difficulty(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
