6b68d7ffcabbb97750e94e4aee5c976ffb37e76d
kayatsa <kayatsa8@gmail.com>
2025-02-27 11:24:23 +0200

    MealGenerator: adding enums for Meal

---
+++ MealGenerator/src/main/java/com/cookbook/mealgenerator/model/enums/MealTime.java
@@ -0,0 +1,5 @@
+package com.cookbook.mealgenerator.model.enums;
+
+public enum MealTime {
+    BREAKFAST, LUNCH, SUPPER, SNACK
+}
---
+++ MealGenerator/src/main/java/com/cookbook/mealgenerator/model/enums/SpecialOccation.java
@@ -0,0 +1,5 @@
+package com.cookbook.mealgenerator.model.enums;
+
+public enum SpecialOccation {
+    HOLIDAY, BIRTHDAY, SHABBAT, FAMILY_EVENT
+}
